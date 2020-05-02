91cefadc6088bad34100ce8edd681c0c105a4541
Jacek Duszenko <jacktheripper@jaceks-macbook-pro.home>
2020-05-02 23:28:51 +0200

    added file download

--- scalafuck/src/main/scala/Main.scala
+++ scalafuck/src/main/scala/Main.scala
@@ -1,3 +1,24 @@
-object Main extends App {
-  println("Hello, World!")
+import scala.io.Source
+
+object Main {
+  val correctNumberOfArgs = 1
+  val usageErrorMessage = "USAGE: scala Main BRAINFUCK_FILE"
+
+  def main(args: Array[String]): Unit = {
+    assert(isArgsNumberCorrect(args), usageErrorMessage)
+    val filename = args.head
+    val code = fileAsString(filename)
+
+    println(code)
+  }
+
+  private def fileAsString(filename: String): String = {
+    val src = Source.fromFile(filename)
+    val result = src.getLines().mkString("\n")
+    src.close()
+    result
+  }
+
+  private def isArgsNumberCorrect(args: Array[String]): Boolean = args.length == correctNumberOfArgs
+
 }
