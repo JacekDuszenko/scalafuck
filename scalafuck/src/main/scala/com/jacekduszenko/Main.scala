680f51b482a70a819e6f8b9b4d9fb4c3273943aa
Jacek Duszenko <jacktheripper@jaceks-macbook-pro.home>
2020-05-03 01:50:31 +0200

    figured out intellij reimport to enable java classes

--- scalafuck/src/main/scala/com/jacekduszenko/Main.scala
+++ scalafuck/src/main/scala/com/jacekduszenko/Main.scala
@@ -2,6 +2,7 @@ package com.jacekduszenko
 
 import com.jacekduszenko.loader.{CodeLoader, FileCodeLoaderType}
 
+
 object Main {
   val correctNumberOfArgs = 1
   val usageErrorMessage = "USAGE: scala Main BRAINFUCK_FILE"
@@ -11,15 +12,20 @@ object Main {
     assert(isArgsNumberCorrect(args), usageErrorMessage)
     val filename = args.head
     val codeLoader: Option[CodeLoader] = CodeLoader.create(FileCodeLoaderType)
-    val contents = codeLoader match {
+    val code = loadCode(filename, codeLoader)
+  }
+
+  private def loadCode(filename: String, codeLoader: Option[CodeLoader]) = {
+    codeLoader match {
       case Some(loader) => loader.loadCode(filename)
-      case None =>
-        println(contentsUnavailableMessage)
-        System.exit(1)
+      case None => abortWithError(usageErrorMessage)
     }
   }
 
+  private def abortWithError(msg: String): Unit = {
+    println(msg)
+    System.exit(1)
+  }
 
   private def isArgsNumberCorrect(args: Array[String]): Boolean = args.length == correctNumberOfArgs
-
 }
