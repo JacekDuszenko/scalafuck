72a73eaaa4f7d8da282069e5c4847d2cbbbb4c9a
Jacek Duszenko <jacktheripper@jaceks-macbook-pro.home>
2020-05-07 01:34:37 +0200

    more functional style of stack

--- scalafuck/src/main/scala/com/jacekduszenko/interpreter/impl/BrainfuckInterpreterImpl.scala
+++ scalafuck/src/main/scala/com/jacekduszenko/interpreter/impl/BrainfuckInterpreterImpl.scala
@@ -4,6 +4,8 @@ import com.jacekduszenko.interpreter.BrainfuckInterpreter
 import com.jacekduszenko.interpreter.impl.BrainfuckInterpreterImpl.{defaultMemorySize, zeroByte}
 import com.jacekduszenko.model.{BrainfuckToken, JumpBack, JumpZero}
 
+import scala.collection.mutable.ArrayBuffer
+
 object BrainfuckInterpreterImpl {
   private val defaultMemorySize = 30000
   private val zero: Int = 0
@@ -47,8 +49,19 @@ case class BrainfuckInterpreterImpl(memorySize: Int = defaultMemorySize) extends
     runtimeState._1(runtimeState._2)
   }
 
+  //TODO refactor to functional => state monad (I know this is ugly, gotta read up more on fp in scala and reimplement)
   def findLoopCode(i: PointerPosition, tokens: List[BrainfuckToken]): List[BrainfuckToken] = {
-    tokens.drop(i + 1).takeWhile(tkn => tkn != JumpBack)
+    var openParenCounter: Int = 0
+    val buffer: ArrayBuffer[BrainfuckToken] = ArrayBuffer()
+    for (t <- tokens.drop(i + 1)) {
+      t match {
+        case JumpBack => openParenCounter -= 1
+        case JumpZero if openParenCounter == 0 => return buffer.toList
+        case JumpZero => openParenCounter += 1; buffer.addOne(t)
+        case _ => buffer.addOne(t)
+      }
+    }
+    buffer.toList
   }
 }
 
